95dbabccb9d1cf879e98bcc877c51848b26dad8a
Roman <roman.deev06@gmail.com>
2022-12-07 03:26:08 +0300

    fix example

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,41 +3,38 @@
 #endif
 
 #include <iostream>
-#include "database.h"
+#include "Database.h"
 
 int main(int argc, char *argv[]) {
-    std::string s = "suka";
-    if (argc <= 1) {
-        std::cout << "Usage: ./example path/to/file";
-        return 0;
-    }
-    try {
-#ifdef DEBUG
-        database db = database(argv[1], true);
-#else
-        database<file_in_memory_interface> db = database<file_in_memory_interface>(argv[1], true);
-#endif
-
-//    db.query({CREATE_SCHEMA, query_payload(
-//            create_schema_query(
-//                    "BOM-BOM",
-//                    {
-//                            {"pole",      DB_INT_32},
-//                            {"bool_pole", DB_BOOL},
-//                    }
-//            )
-//    )});
-//    for (auto now: db.get_schemas()) {
-//        
-//    }
-//
-//    char lol[100];
-//    debug(db.file->read(lol, 100, 0));
-//    debug(feof(db.file->fd));
-//    debug(lol);
-    }
-    catch (const char *msg) {
-        std::cout << msg << std::endl;
-    }
+  std::string s = "suka";
+  if (argc <= 1) {
+    std::cout << "Usage: ./example path/to/file";
     return 0;
+  }
+  Database db = Database(argv[1], true);
+  db.Query({CREATE_SCHEMA, query_payload(
+      create_schema_query(
+          "BOM-BOM-BOM-BOM",
+          {
+              {"pole_poolyushko", DB_INT_32},
+              {"bool_poolyushko", DB_BOOL},
+          }
+      )
+  )});
+  db.Query({CREATE_SCHEMA, query_payload(
+      create_schema_query(
+          "SUKO-SUKOO-SUKO",
+          {
+              {"AHAHA-AHA-AHAHA", DB_FLOAT},
+              {"OLOLOLO-OLOLOLO", DB_STRING},
+          }
+      )
+  )});
+  auto kek = db.GetSchemas();
+  auto lol = std::get<std::vector<Schema>>(kek.payload_);
+
+  for (auto now : lol) {
+    now.Print();
+  }
+  return 0;
 }
