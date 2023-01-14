5e9d2c956bb7dfb577ef29975d4929ec2fe625e0
Roman <roman.deev06@gmail.com>
2023-01-15 00:20:55 +0300

    add file size bench

--- llp/tests/bench.cpp
+++ llp/tests/bench.cpp
@@ -142,3 +142,56 @@ TEST(BenchTest, InsertsAndRemove) {
     last_time = now;
   }
 }
+
+TEST(BenchTest, InsertsAndRemoveFileSize) {
+  std::ios_base::sync_with_stdio(false);
+  Database db = Database("../tmp/file.hex", true);
+
+  db.CreateSchema({"BOM-BOM-BOM-BOM",
+                   {
+                       {"pole_poolyushko", DB_INT_32},
+                       {"bool_poolyushko", DB_BOOL},
+                   }});
+  db.CreateSchema({"SCHEMKA-SCHEMKA",
+                   {
+                       {"AHAHA-AHA-AHAHA", DB_DOUBLE},
+                       {"OLOLOLO-OLOLOLO", DB_STRING},
+                   }});
+  db.CreateSchema({"WEWE-WEWEE-WEWE",
+                   {
+                       {"12345-678-90123", DB_DOUBLE},
+                       {"7654321-1234567", DB_STRING},
+                   }});
+  auto start_time = GetTime();
+  auto last_time = start_time;
+  auto root_id = get<int64_t>(db.InsertElement({0,
+                                                "SCHEMKA-SCHEMKA",
+                                                {
+                                                    {"AHAHA-AHA-AHAHA", 3.14},
+                                                    {"OLOLOLO-OLOLOLO", "URA, INSERT"},
+                                                }})
+                                  .payload_);
+  for (int i = 0; i < 20; i++) {
+    for (int j = 0; j < 10000; j++) {
+      db.InsertElement({root_id,
+                        "SCHEMKA-SCHEMKA",
+                        {
+                            {"AHAHA-AHA-AHAHA", last_time},
+                            {"OLOLOLO-OLOLOLO", "URA, INSERT" + std::to_string(j)},
+                        }});
+      db.InsertElement({root_id,
+                        "SCHEMKA-SCHEMKA",
+                        {
+                            {"AHAHA-AHA-AHAHA", 3.14},
+                            {"OLOLOLO-OLOLOLO", "URA, INSERT" + std::to_string(j)},
+                        }});
+    }
+    last_time = GetTime();
+    if(i >= 5) {
+      db.DeleteElements({"SCHEMKA-SCHEMKA", {{"AHAHA-AHA-AHAHA", OP_EQUAL, 3.14}}});
+    }
+    auto now = GetTime();
+    std::cout << (i + 1) * 10000 + 1 << '\t' << now - last_time << '\t' << db.GetFileSize() << std::endl;
+    last_time = now;
+  }
+}
