0adc08ed1c83d97e433b0e6584074c97a168179b
Roman <roman.deev06@gmail.com>
2023-01-28 07:52:19 +0300

    lab3 report

--- parser/src/main.cpp
+++ parser/src/main.cpp
@@ -1,10 +1,6 @@
 extern "C" {
 #include "parser.h"
 }
-#include "message.pb.h"
-#include <iostream>
 int main() {
-  message::Message m;
-  std::cout << "Hello Proto!" << std::endl;
   return yyparse();
 }
